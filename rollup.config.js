95565089c177f185e88c48208f246e85a56ad8ef
yugasun <yuga_sun@163.com>
2019-07-11 10:31:47 +0800

    fix: remove unuse file

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    input: path.join(pkg.src, 'index.js'),
-    plugins: [
-        babel({
-            runtimeHelpers: true,
-            exclude: 'node_modules/**',
-        }),
-    ],
-};
